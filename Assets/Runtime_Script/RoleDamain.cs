5640ff83bbf7a6e80ddc17d7ed9fc8be0244690e
lazyihui <chengbaozi6@qq.com>
2024-07-10 23:55:50 +0800

    <WIP>

--- Assets/Runtime_Script/RoleDamain.cs
+++ Assets/Runtime_Script/RoleDamain.cs
@@ -6,6 +6,9 @@ public static class RoleDamain
 {
     public static void Move(RoleEntity enemy,Vector3 centerPos ,float dt)
     {
-        enemy.transform.RotateAround(centerPos, new Vector3(0,0,1), 60*dt);
+        float distance = Vector3.Distance(enemy.transform.position, centerPos);
+        Debug.Log("distance:"+distance);
+        float angle = 1/180*distance;
+        enemy.transform.RotateAround(centerPos, new Vector3(0,0,1), distance*180 * dt);
     }
 }
