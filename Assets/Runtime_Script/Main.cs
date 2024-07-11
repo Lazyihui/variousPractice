2002fe1e113c2db8919e999c2f43f16d000c9802
lazyihui <chengbaozi6@qq.com>
2024-07-11 18:08:54 +0800

    <WIP>函数用到了不可以用的数据

--- Assets/Runtime_Script/Main.cs
+++ Assets/Runtime_Script/Main.cs
@@ -7,28 +7,32 @@ public class Main : MonoBehaviour {
 
     [SerializeField] RoleEntity enmey;
 
-    Vector3 CenterPos = new Vector3(0, 0, 0);
-    Vector3 radius;
+    Vector3 center = new Vector3(0,0,0);
+    
 
     void Awake() {
-        CenterPos = boss.transform.position;
-        radius = enmey.transform.position - CenterPos;
+        
     }
-
-    public Vector3 Rotate(Vector3 centerPos, Vector3 rolePos/*要移动物体的pos*/, float round/*以每秒多少度的速度绕半径向量旋转*/, float dt) {
-        Vector3 radius = rolePos - centerPos;
-        radius = Quaternion.AngleAxis(round * dt, Vector3.forward) * radius;
-        return centerPos + radius;
-    }
-
     void Update() {
         float dt = Time.deltaTime;
-        // RoleDamain.Move(enmey, boss.transform.position, dt);
-        CenterPos = boss.transform.position;
 
-        radius = Quaternion.AngleAxis(180*dt,Vector3.forward)*radius;
-        enmey.transform.position = CenterPos + radius;
-        // enmey.transform.position = Rotate(boss.transform.position, enmey.transform.position, 180, dt);
+        AroundRotare(dt,180,center,enmey.transform.position);
 
     }
+
+
+
+    public void AroundRotare(float dt,float round,Vector3 centerPos,Vector3 RolePos) {
+        
+
+        center = boss.transform.position;
+        Vector3 radius = RolePos - centerPos;
+
+
+        radius = Quaternion.AngleAxis(round * dt, Vector3.forward) * radius;
+        enmey.transform.position = center + radius;
+
+    }
+
+
 }
