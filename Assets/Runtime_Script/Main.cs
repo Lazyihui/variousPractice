a1afe12f13222d3166e7943969ce31a5314810a8
lazyihui <chengbaozi6@qq.com>
2024-07-11 17:34:15 +0800

    <feature>add ： 一个物体绕着移动的物体旋转（还没有写成函数）

--- Assets/Runtime_Script/Main.cs
+++ Assets/Runtime_Script/Main.cs
@@ -2,26 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour
-{
+public class Main : MonoBehaviour {
     [SerializeField] RoleEntity boss;
 
     [SerializeField] RoleEntity enmey;
 
     Vector3 CenterPos = new Vector3(0, 0, 0);
-    Vector3  radius;
+    Vector3 radius;
 
-    void Awake()
-    {
-        radius = boss.transform.position - CenterPos;
+    void Awake() {
+        CenterPos = boss.transform.position;
+        radius = enmey.transform.position - CenterPos;
     }
 
-    void Update()
-    {
+    public Vector3 Rotate(Vector3 centerPos, Vector3 rolePos/*要移动物体的pos*/, float round/*以每秒多少度的速度绕半径向量旋转*/, float dt) {
+        Vector3 radius = rolePos - centerPos;
+        radius = Quaternion.AngleAxis(round * dt, Vector3.forward) * radius;
+        return centerPos + radius;
+    }
+
+    void Update() {
         float dt = Time.deltaTime;
         // RoleDamain.Move(enmey, boss.transform.position, dt);
+        CenterPos = boss.transform.position;
 
         radius = Quaternion.AngleAxis(180*dt,Vector3.forward)*radius;
-        boss.transform.position = CenterPos + radius;
+        enmey.transform.position = CenterPos + radius;
+        // enmey.transform.position = Rotate(boss.transform.position, enmey.transform.position, 180, dt);
+
     }
 }
