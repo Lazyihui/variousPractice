618ea31ef13aa117f08a67249740091a26f70bf6
lazyihui <chengbaozi6@qq.com>
2024-07-11 17:23:29 +0800

    <feature>用向量的旋转 第二种

--- Assets/Runtime_Script/Main.cs
+++ Assets/Runtime_Script/Main.cs
@@ -8,14 +8,20 @@ public class Main : MonoBehaviour
 
     [SerializeField] RoleEntity enmey;
 
+    Vector3 CenterPos = new Vector3(0, 0, 0);
+    Vector3  radius;
+
     void Awake()
     {
-
+        radius = boss.transform.position - CenterPos;
     }
 
     void Update()
     {
         float dt = Time.deltaTime;
-        RoleDamain.Move(enmey, boss.transform.position, dt);
+        // RoleDamain.Move(enmey, boss.transform.position, dt);
+
+        radius = Quaternion.AngleAxis(180*dt,Vector3.forward)*radius;
+        boss.transform.position = CenterPos + radius;
     }
 }
